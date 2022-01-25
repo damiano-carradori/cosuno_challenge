60d6cdae99da99c31c3c6c0cd2307a3e20aed6bd
Damiano Carradori <damiano.carradori@gmail.com>
2022-01-25 16:51:54 +0100

    feat: added cors

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -2,9 +2,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var companiesRouter = require("./routes/companies");
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
 
 var app = express();
 
@@ -13,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 app.use("/companies", companiesRouter);
